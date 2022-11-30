917cdba952b531cd86295ec1f65ffb90309e20d7
tessico <tessico@protonmail.com>
2022-11-30 16:41:09 +0100

    remove SIG tag, replaced by `SCHNORR_SIG`

--- src/lib.rs
+++ src/lib.rs
@@ -98,7 +98,6 @@ pub mod jellyfish {
     /// Tags to be used for [`tagged-base64`](https://github.com/espressosystems/tagged-base64)
     pub mod tag {
         pub const VERKEY: &str = "VERKEY";
-        pub const SIG: &str = "SIG";
         pub const NODE: &str = "NODE";
         pub const LEAF: &str = "LEAF";
         pub const PROOF: &str = "PROOF";
